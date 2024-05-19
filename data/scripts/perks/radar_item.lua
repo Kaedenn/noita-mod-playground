0f636659bcc9ac1a8afa590443a10e2fcaecc266
Kaedenn <kaedenn@gmail.com>
2024-05-19 14:50:51 -0800

    Add special container radar icon

--- data/scripts/perks/radar_item.lua
+++ data/scripts/perks/radar_item.lua
@@ -16,6 +16,12 @@ local sprites = {
 	"mods/kae_test/files/particles/radar_container_strong.png",
 }
 
+local sprite_bases = {
+	["item"] = 1,
+	["heart"] = 4,
+	["container"] = 7,
+}
+
 local entity_id = GetUpdatedEntityID()
 local pos_x, pos_y = EntityGetTransform( entity_id )
 pos_y = pos_y - 4 -- offset to middle of character
@@ -23,24 +29,30 @@ pos_y = pos_y - 4 -- offset to middle of character
 local range = 400
 local indicator_distance = 22
 
+function classify_item(entid)
+	local fname = EntityGetFilename(entid)
+	if fname:match("heart.xml") then
+		return "heart"
+	end
+	if EntityHasTag(entid, "potion") or EntityHasTag(entid, "powder_stash") then
+		return "container"
+	end
+	return "item"
+end
+
 function radar_get_items(px, py, r, tags)
 	local nearby = {}
 	for _, tag in ipairs(tags) do
 		for _, entid in pairs(EntityGetInRadiusWithTag(px, py, r, tag)) do
 			if not nearby[entid] then
-				local fname = EntityGetFilename(entid)
-				if fname:match("heart.xml") then
-					nearby[entid] = "heart"
-				else
-					nearby[entid] = "item"
-				end
+				nearby[entid] = classify_item(entid)
 			end
 		end
 	end
 	return nearby
 end
 
--- ping nearby wand
+-- ping nearby items
 for id, kind in pairs(radar_get_items(pos_x, pos_y, range, {"item_pickup"})) do
 	local wand_x, wand_y = EntityGetTransform(id)
 	local parent = EntityGetRootEntity( id );
@@ -56,10 +68,7 @@ for id, kind in pairs(radar_get_items(pos_x, pos_y, range, {"item_pickup"})) do
 		local indicator_x = pos_x + dir_x * indicator_distance
 		local indicator_y = pos_y + dir_y * indicator_distance
 
-		local sprite_base = 1
-		if kind == "heart" then
-			sprite_base = 4
-		end
+		local sprite_base = sprite_bases[kind] or 1
 
 		-- display sprite based on proximity
 		if distance > range * 0.5 then
